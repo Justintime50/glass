fd077fb43152cc0166947c03f27490b8a34beb0a
Justintime50 <justinpaulhammond@gmail.com>
2019-06-25 23:36:16 -0600

    Working Laravel install in docker

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    return view('admin');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
