780104877535dba449327cf7ef144552d7c98eee
Justin Hammond <39606064+Justintime50@users.noreply.github.com>
2021-02-08 10:22:42 -0700

    Replacing slugify composer package with npm package (#43)
    * Replacing slugify composer package with npm package
    
    * Fix slugify, bump dependencies
    
    * Undoes bad order change to app.blade.php

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -19,7 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -30,3 +30,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+/**
+ * Place custom Node Modules below
+ */
+window.slugify = require('@sindresorhus/slugify')
