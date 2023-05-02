68056d007b5b7390d6dfb956ae6e64e0d4a24a28
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-05-02 12:29:11 -0600

    fix: split sass

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,4 +1,3 @@
-import "@popperjs/core";
 import "bootstrap";
 
 import slugify from "@sindresorhus/slugify";
@@ -13,17 +12,10 @@ import * as glass from "./glass";
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
 window.axios = axios;
-
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-/**
- * Place custom Node Modules below
- */
 hljs.highlightAll();
 window.slugify = slugify;
 window.pineapple = pineapple;
-
-// Custom app Javascript
 window.app = glass;
