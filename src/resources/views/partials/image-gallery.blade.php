86cf46f7e0bc992fb427a64d9d67d6e54f32a4bf
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2024-06-13 13:49:21 -0600

    fix: use chunks instead of homegrown rows for image gallery

--- src/resources/views/partials/image-gallery.blade.php
+++ src/resources/views/partials/image-gallery.blade.php
@@ -1,32 +1,29 @@
-<div class="row image-row-container">
-    @foreach ($images as $index => $image)
-        <div class="col-md-4 image-page-image-container text-center">
-            <div class="flex-center-container">
-                <img src='{{ \App\Http\Controllers\ImageController::getImageAssetPath($image->subdirectory, $image->filename) }}'
-                    class="img-thumbnail image-preview">
-            </div>
-            <div class="auto-margin-top">
-                <p>{{ $image->filename }}</p>
-                @if (str_contains(Request::url(), 'images'))
-                    <form action="/images/{{ $image->id }}" method="POST">
-                        @csrf
-                        @method('DELETE')
+@foreach ($images->chunk(3) as $chunk)
+    <div class="row image-row-container">
+        @foreach ($chunk as $image)
+            <div class="col-md-4 image-page-image-container text-center">
+                <div class="flex-center-container">
+                    <img src='{{ \App\Http\Controllers\ImageController::getImageAssetPath($image->subdirectory, $image->filename) }}'
+                        class="img-thumbnail image-preview">
+                </div>
+                <div class="auto-margin-top">
+                    <p>{{ $image->filename }}</p>
+                    @if (str_contains(Request::url(), 'images'))
+                        <form action="/images/{{ $image->id }}" method="POST">
+                            @csrf
+                            @method('DELETE')
 
-                        <a class="btn btn-primary btn-sm"
-                            href="{{ \App\Http\Controllers\ImageController::getImageAssetPath($image->subdirectory, $image->filename) }}"
-                            download>Download</a>
-                        <input type="submit" value="Delete" class="btn btn-sm btn-danger">
-                    </form>
-                @else
-                    <a class="btn btn-sm btn-primary" data-bs-dismiss="modal"
-                        onclick='app.selectImage("{{ $image->id }}", "{{ $image->filename }}")'>Select</a>
-                @endif
+                            <a class="btn btn-primary btn-sm"
+                                href="{{ \App\Http\Controllers\ImageController::getImageAssetPath($image->subdirectory, $image->filename) }}"
+                                download>Download</a>
+                            <input type="submit" value="Delete" class="btn btn-sm btn-danger">
+                        </form>
+                    @else
+                        <a class="btn btn-sm btn-primary" data-bs-dismiss="modal"
+                            onclick='app.selectImage("{{ $image->id }}", "{{ $image->filename }}")'>Select</a>
+                    @endif
+                </div>
             </div>
-        </div>
-
-        @if (($index + 1) % 3 == 0)
-</div>
-<div class="row image-row-container">
-    @endif
-    @endforeach
-</div>
+        @endforeach
+    </div>
+@endforeach
