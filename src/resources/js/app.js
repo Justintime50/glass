fb9b93d187e3f52c023be2665cab099f902211b9
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2024-09-26 09:47:17 -0600

    chore: remove axios

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,19 +1,10 @@
 import "bootstrap";
 
 import slugify from "@sindresorhus/slugify";
-import axios from "axios";
 import hljs from "highlight.js";
 import pineapple from "pineapple-library/pineapple/dist/js/pineapple";
 
 import * as glass from "./glass";
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-window.axios = axios;
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 hljs.highlightAll();
 window.slugify = slugify;
